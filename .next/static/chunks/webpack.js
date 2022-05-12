51c91d5b6d28662fa090308558314d8b30691fa0
shaunvan119 <shaunvan@live.com.au>
2022-05-12 12:38:58 +1000

    completed hero section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7851e1d148a61d55f037"; }
+/******/ 		__webpack_require__.h = function() { return "2e5935720f14f9ee53b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To<br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Software Engineering | Frontend Developer | Backend | React Developer | Next js | JavaScript
+      </SectionText>
+      <Button onClick={() => window.location = 'https://www.linkedin.com/in/shaun-van-amersfoort-80b7b7194/'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
